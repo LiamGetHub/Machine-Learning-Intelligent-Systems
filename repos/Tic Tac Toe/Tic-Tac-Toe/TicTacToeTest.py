4706ba1f50c44edb2739e2cdc6ff80f88b8d4c06
Liam <157172169+LiamGetHub@users.noreply.github.com>
2025-04-29 07:53:22 -0700

    Delete repos directory

--- repos/Tic Tac Toe/Tic-Tac-Toe/TicTacToeTest.py
+++
@@ -1,7 +0,0 @@
-from TicTacToe import *
-
-if __name__ == "__main__":
-    print("Tic Tac Toe")
-
-    game = TicTacToe()
-    game.play()
--- repos/Tic Tac Toe/tictactoe-2025/test tic-tac-toe.py
+++
@@ -1,7 +0,0 @@
-from tictactoe import *
-
-if __name__ == "__main__":
-    print("Tic Tac Toe")
-
-    game = TicTacToe()
-    game.play()
